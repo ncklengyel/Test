f9f5510901ab9a05a6db7881453af887eff9c8a5
Lengyel <am37580@logti-a3412-47.ens.ad.etsmtl.ca>
2016-04-06 21:03:51 -0400

    listAC in progress

--- src/test/test.java
+++ src/test/test.java
@@ -1,5 +1,7 @@
 package test;
 
+import java.util.ArrayList;
+
 public class test {
 
 	public static void main(String[] args) {
@@ -14,13 +16,31 @@ public class test {
 					 {22,35,199,200,191,187,187,175},
 					 {36,200,200,200,188,185,187,186}};
 		
+		int[] mat1D = {6,2,0,0,15,0,0,29,0,1,0,0,0,0,0,0,0,0,1};
+		
 	
 		int[][] mat2 = Zig_Zag(8);
 		
-		Zig_Zag2(mat2);
+		//System.out.println(numberOfZeros(mat1D, 10));
+
+		//System.out.println(indexOfEOB(mat1D));
+
 		//print2d(mat2);
 		//System.out.println();
 		//zigzag(mat2);
+		
+		//System.out.println(listAC(mat1D).toString());
+		
+		ArrayList<int[]> listAC = listAC(mat1D);
+		
+		for(int[] temp : listAC){
+			
+			for (int i = 0; i < temp.length; i++) {
+				System.out.print(temp[i]+" ");
+			}
+			System.out.println();
+			
+		}
 		
 	}
 	
@@ -55,7 +75,7 @@ public class test {
 	
 	//https://rosettacode.org/wiki/Zig-zag_matrix (a été modifier pour print plutot que remplir)
 	//Print un array en zigzag, array 2d doit etre symétrique
-	public static int[][] Zig_Zag2(int[][] data) {
+	public static void  Zig_Zag2(int[][] data) {
 		
 		int i = 1;
 		int j = 1;
@@ -81,7 +101,40 @@ public class test {
 					j--;
 			}
 		}
-		return data;
+
+	}
+	
+	public static int[] Zig_ZagtoArray(int[][] data) {
+		
+		int[] mat = new int [data.length*data.length];
+		
+		int i = 1;
+		int j = 1;
+		for (int element = 0; element < data.length * data.length; element++) {
+			
+			mat[element] = data[i-1][j-1];
+			
+			if ((i + j) % 2 == 0) {
+				// Even stripes
+				if (j < data.length)
+					j++;
+				else
+					i += 2;
+				if (i > 1)
+					i--;
+			} else {
+				// Odd stripes
+				if (i < data.length)
+					i++;
+				else
+					j += 2;
+				if (j > 1)
+					j--;
+			}
+		}
+		
+		return mat;
+
 	}
 
 
@@ -96,6 +149,97 @@ public class test {
 			}
 			System.out.println();
 		}
+		
+	}
+	
+	public static void print1D(int[] mat) {
+		for (int i = 0; i < mat.length; i++) {
+			System.out.print(mat[i]+" ");
+		}
+	}
+	
+	//return the EOB de l'array
+	public static int indexOfEOB(int[] mat){
+		
+		int index = -1;
+		
+		for (int i = mat.length-1; i >= 0; i--) {
+			
+			if (mat[i]!=0) {
+				
+				index=i;
+				index=index+1;
+				break;
+				
+			}
+			
+		}
+		
+		return index;
+		
+	}
+	
+	public static ArrayList<int[]> listAC (int[] mat){
+		
+		ArrayList<int[]> listAc = new ArrayList<>();
+		
+		
+		
+		int indexEOB = indexOfEOB(mat);
+		
+		//System.out.println(indexEOB);
+		//System.out.println(mat.length);
+		
+		for (int i = 1; i < indexEOB ; i++) {
+			
+			if (mat[i]!=0) {
+				
+				int[] arrayAc = {0,mat[i]};
+				listAc.add(arrayAc);
+				
+
+			}else{
+				
+				int[] arrayAC2 = new int [2];
+				arrayAC2[0] = numberOfZeros(mat, i);
+				arrayAC2[1] = mat[arrayAC2[0]];
+				i=i+arrayAC2[0];
+				listAc.add(arrayAC2);
+
+			}
+			
+			
+		}
+		
+		if (indexEOB != mat.length) {
+			
+			int[] arrayEOB = {0,0};
+			listAc.add(arrayEOB);
+			
+		}
+		
+		return listAc;
+		
+		
+	}
+	
+	public static int numberOfZeros(int[] mat,int index){
+		
+		int numZero = 0;
+		
+		for (int i = index; i < mat.length; i++) {
+			
+			if(mat[i]!=0){
+				
+				break;
+				
+			}
+			
+			numZero++;
+			
+		}
+		
+		return numZero;
 		
 	}
 
